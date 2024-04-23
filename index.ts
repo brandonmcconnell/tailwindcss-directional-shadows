9f9f27644811dfe1b13c480846c158c0c46115dc
Brandon McConnell <brandon@dreamthinkbuild.com>
2024-04-23 03:12:04 -0700

    Make type safe

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import plugin from 'tailwindcss/plugin';
+import plugin from 'tailwindcss/plugin.js';
 
 export default plugin(function ({ addUtilities, matchUtilities, theme }) {
   // Directional shadows
