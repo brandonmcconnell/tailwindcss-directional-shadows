d67dab53756f7f89063f772f84e20810d5edf424
Brandon McConnell <brandon@dreamthinkbuild.com>
2024-08-22 17:27:12 -0400

    Add support for `x`, `y`, and `*` (all) directions

--- index.ts
+++ index.ts
@@ -1,26 +1,32 @@
 import plugin from 'tailwindcss/plugin.js';
 
-export default plugin(function ({ addUtilities, matchUtilities, theme }) {
+export default plugin(function ({ addUtilities, matchUtilities, theme, e }) {
   // Directional shadows
-  ['sm', '', 'md', 'lg', 'xl', '2xl'].forEach((suffix) => {
-    ['b', 'r', 't', 'l'].forEach((dir, i) => {
-      const isEven = i % 2 === 0;
-      const baseStyleString = theme(`boxShadow.${suffix || 'DEFAULT'}`) as string;
-      const styleString = baseStyleString
-        .split(', ')
-        .map((basePart: string) =>
-          basePart
-            .split(' ')
-            .slice(0, 2)
-            .map((l, j) => (j === 1 && i > 1 ? `-${l}` : l))
-            [isEven ? 'slice' : 'reverse']()
-            .concat(basePart.split(' ').slice(2, 4))
-            .join(' ')
-        )
-        .map((p) => `${p} var(--tw-shadow-color, rgb(0 0 0 / 0.1))`)
-        .join(', ');
+  const isVertical = (dir: string) => dir === 't' || dir === 'b';
+  const useNegative = (dir: string) => dir === 'l' || dir === 't';
+  function processDir(dir: string, size: string) {
+    const baseStyleString = theme(`boxShadow.${size || 'DEFAULT'}`) as string;
+    return baseStyleString
+      .split(', ')
+      .map((basePart: string) =>
+        basePart
+          .split(' ')
+          .slice(0, 2)
+          .map((l, j) => (j === 1 && useNegative(dir) ? `-${l}` : l))
+          [isVertical(dir) ? 'slice' : 'reverse']()
+          .concat(basePart.split(' ').slice(2, 4))
+          .join(' ')
+      )
+      .map((p) => `${p} var(--tw-shadow-color, rgb(0 0 0 / 0.1))`)
+      .join(', ');
+  }
+  ['sm', '', 'md', 'lg', 'xl', '2xl'].forEach((size) => {
+    ['t', 'r', 'b', 'l', 'x', 'y', '*'].forEach((dir: string) => {
+      const dirs = dir === '*' ? ['t', 'r', 'b', 'l'] : dir === 'x' ? ['l', 'r'] : dir === 'y' ? ['t', 'b'] : [dir];
+      const styleStrings = dirs.map((dir: string) => processDir(dir, size));
+      const styleString = styleStrings.join(', ');
       addUtilities({
-        [`.shadow-${dir}${suffix ? `-${suffix}` : ''}`]: {
+        [`.${e(`shadow-${dir}${size ? `-${size}` : ''}`)}`]: {
           '--tw-shadow': styleString,
           '--tw-shadow-colored': styleString,
           'box-shadow': 'var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow)',
